d04535e16d10e0ec12925c89ce0366208f8a9df2
constayush <aayush@mail.com>
2024-03-30 00:19:09 +0530

    header changes

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -2,25 +2,33 @@ import React from 'react'
 import willSmith from "../../../public/assets/willsmith.jpg"
 function Navbar() {
     return (
-        <div className='w-full lg:h-[3rem] flex justify-between items-center bg-white p-[2rem]'>
+        <div className='w-full lg:h-[3rem] flex justify-around items-center bg-white p-[2rem]'>
 
 
             <div className='logo '>Sociality</div>
 
 
+
             <div className='stories-container'>
 
                 <ul className='flex justify-center items-center'>
-                    <li className='story-container'><img className='w-[50px] ' src={willSmith}/></li>
-                    <li className='story-container'><img className='w-[50px] ' src={willSmith}/></li>
-                    <li className='story-container'><img className='w-[50px] ' src={willSmith}/></li>
-                    <li className='story-container'><img className='w-[50px] ' src={willSmith}/></li>
-                    <li className='story-container'><img className='w-[50px] ' src={willSmith}/></li>
-                    <li className='story-container'><img className='w-[50px] ' src={willSmith}/></li>
-                    <li className='story-container'><img className='w-[50px] ' src={willSmith}/></li>
+                    <li className='story-container mx-[1rem]'><img className='w-[50px] ' src={willSmith}/></li>
+                    <li className='story-container mx-[1rem]'><img className='w-[50px] ' src={willSmith}/></li>
+                    <li className='story-container mx-[1rem]'><img className='w-[50px] ' src={willSmith}/></li>
+                    <li className='story-container mx-[1rem]'><img className='w-[50px] ' src={willSmith}/></li>
+                    <li className='story-container mx-[1rem]'><img className='w-[50px] ' src={willSmith}/></li>
+                    <li className='story-container mx-[1rem]'><img className='w-[50px] ' src={willSmith}/></li>
+                    <li className='story-container mx-[1rem]'><img className='w-[50px] ' src={willSmith}/></li>
                 </ul>
 
             </div>
+
+
+<div className='searchBar-container'>
+
+<input type='text' />
+
+</div>
 
 
 
